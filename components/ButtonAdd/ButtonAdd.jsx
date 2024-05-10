12fb43441a78dd9968f3bd79c1e501ae70b925de
WilsonReese <71947861+WilsonReese@users.noreply.github.com>
2024-05-10 21:41:46 +0000

    created ButtonAdd component and style

---
+++ components/ButtonAdd/ButtonAdd.jsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { s } from "./TabBottomMenu.style";
+
+export function ButtonAdd({  }) {
+  return (
+    <View>
+        <Text>Hello.</Text>
+    </View>
+  );
+}
